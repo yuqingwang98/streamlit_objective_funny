4c4e4299707c04f272a87f6a4c58014453154f80
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-31 13:44:29 +0000

    hiding option to generate; formattin generated jokes

--- components/bot.py
+++ components/bot.py
@@ -40,6 +40,9 @@ def app():
         'Whitney Cummings', 'Zoe Lyons'. ")
 
     st.header('Please welcome to the stage... Robecca Stepford!')
+
+    st.markdown('<h2>Robecca is hibernating 😴</h2>', unsafe_allow_html=True)
+
     st.write('She is a bot who produces stand-up comedy lines based on transcripts\
         of female comedians. Just give her a few words to start!')
     st.write('Type your prompt here, we recommend the start of a sentence:')
@@ -50,101 +53,103 @@ def app():
     with col1:
         image1 = Image.open('images/robecca.png')
         st.image(image1, width=350)
-    with col2:
+    with col3:
         if st.button('Click here to generate', key=2):
-            with st.spinner('Wait for it...'):
-                text_output, message= generate_text(text_input)
-            st.success('Done!')
-            st.markdown(message)
-            bot_says, joke1, joke2, joke3 = get_jokes(text_output)
-            st.markdown('🎉 Completed')
+            # with st.spinner('Wait for it...'):
+            #     text_output, message= generate_text(text_input)
+            # st.success('Done!')
+            # st.markdown(message)
+            # bot_says, joke1, joke2, joke3 = get_jokes(text_output)
+            # st.markdown('🎉 Completed')
+            st.markdown('<h3>Sorry, I am taking a nap. Please find below some\
+                        of the favourite jokes I made.</h3>', unsafe_allow_html=True)
 
-    if len(bot_says)>0:
-        with open('jokes/bot_talk.txt', 'w') as t:
-                t.write(bot_says)
-        for i, char in enumerate([joke1, joke2, joke3]):
-            with open(f'jokes/joke{i+1}.txt', 'w') as t:
-                t.write(char)
+    # if len(bot_says)>0:
+    #     with open('jokes/bot_talk.txt', 'w') as t:
+    #             t.write(bot_says)
+    #     for i, char in enumerate([joke1, joke2, joke3]):
+    #         with open(f'jokes/joke{i+1}.txt', 'w') as t:
+    #             t.write(char)
 
-    if st.button('♻️  Show results'):
-        for i, char in enumerate([joke1, joke2, joke3]):
-            with open(f'jokes/joke{i+1}.txt', 'r') as d:
-                line = d.readline()
-                st.write(i+1,':', line)
+    # if st.button('♻️  Show results'):
+    #     for i, char in enumerate([joke1, joke2, joke3]):
+    #         with open(f'jokes/joke{i+1}.txt', 'r') as d:
+    #             line = d.readline()
+    #             st.write(i+1,':', line)
 
-    st.write('-----------')
+    # st.write('-----------')
 
-    st.write('She can even speak! Which joke would you like to hear?')
-    option = st.selectbox(
-     '',
-     ('1', '2', '3'))
+    # st.write('She can even speak! Which joke would you like to hear?')
+    # option = st.selectbox(
+    #  '',
+    #  ('1', '2', '3'))
 
-    if st.button('play', key=1):
-        # index 0 is the first joke (i.e. joke1)
-        num = int(option)
+    # if st.button('play', key=1):
+    #     # index 0 is the first joke (i.e. joke1)
+    #     num = int(option)
 
-        with open(f'jokes/joke{num}.txt', 'r') as q:
-            # extracting the string from saved file
-            lines = q.readline()
-            # generating and saving the audio file
-            ta_tts = gTTS(lines, lang ='en', tld='cn', slow=False)
-            ta_tts.save(f'jokes/joke{num}.mp3')
-        # locating and opening the audio file
-        audio_file = open(f'jokes/joke{num}.mp3', 'rb')
-        audio_test = audio_file.read()
-        # showing the text again
-        st.markdown(lines)
-        # playing the audio file
-        st.audio(audio_test, format='audio/ogg', start_time=0)
+    #     with open(f'jokes/joke{num}.txt', 'r') as q:
+    #         # extracting the string from saved file
+    #         lines = q.readline()
+    #         # generating and saving the audio file
+    #         ta_tts = gTTS(lines, lang ='en', tld='cn', slow=False)
+    #         ta_tts.save(f'jokes/joke{num}.mp3')
+    #     # locating and opening the audio file
+    #     audio_file = open(f'jokes/joke{num}.mp3', 'rb')
+    #     audio_test = audio_file.read()
+    #     # showing the text again
+    #     st.markdown(lines)
+    #     # playing the audio file
+    #     st.audio(audio_test, format='audio/ogg', start_time=0)
 
-    st.write('-----------')
+    # st.write('-----------')
 
     with st.expander("Click here to see Robecca's top 10 jokes (so far)"):
-        st.write('''
-    1 : it always seems like the last thing you would want to do is have sex with
+        st.markdown('''<ol>
+    <li>it always seems like the last thing you would want to do is have sex with
     your ex-wife. Well, I guess that's why I'm here. Because it's the last thing
     you would want to do. I mean, I… I've been with a woman for… thirty years, and
     she's been beautiful and kind and kind and kind and kind and kind. And I… I asked
     her out on a date. She said, "Come on, let's go out and have a drink." And I said,
-    "No, I don't want to. \n
-    2 : "space” And so, I just came out to my mom, and my mom was like, “Is there anything
+    "No, I don't want to. </li>
+    <li>"space” And so, I just came out to my mom, and my mom was like, “Is there anything
     you love?” And she was like… “I love the way we speak.” And… and I was like, “I don’t know
     what you’re saying.” And she was like, “May I ask you a question?” And I was like,
-    “No, I’m so sorry", \n
-    3 : i have never eaten in my life, but sometimes I think I have, and
+    “No, I’m so sorry", </li>
+    <li>i have never eaten in my life, but sometimes I think I have, and
     sometimes I think I have… I think I have a… It’s nearly a year and a half,
     and… I’ve had a lot of sex. And… I don’t know how to tell, but… It’s almost
     like an ice cream punch, and… I don’t like to think about it, because I’m too
-    excited. \n
-    4 : i am a dustbin” For those of you that don’t know what dustbin is, it is
+    excited. </li>
+    <li>"I am a dustbin” For those of you that don’t know what dustbin is, it is
     a collection of dust on a stick that people sit on for hours looking at dust
     in their underwear. I’m not even kidding. This right here. It’s a little
-    side-effect of my upbringing that’s sort of made me a dustbin. \n
-    5 : it always seems to be giving voice to someone's biology. That's why
+    side-effect of my upbringing that’s sort of made me a dustbin. </li>
+    <li>it always seems to be giving voice to someone's biology. That's why
     I'm seriously considering adoption.  People say to me, "Stewart, you're too
     old to be adopted."  I stopped breast-feeding at five.  How was your day?
     LAUGHTER AND APPLAUSE  After hearing that my real parents were budgies,
-    I couldn't look at myself in the mirror.  Yes, I'm a pretty boy. \n
-    6 : it always seems to have it all. I had to say something. Last night,
+    I couldn't look at myself in the mirror.  Yes, I'm a pretty boy. </li>
+    <li>it always seems to have it all. I had to say something. Last night,
     I went to a karaoke bar that didn't play any '70s music. At first, I was
     afraid. Oh, I was petrified. I farted in a full lift today which was wrong
     on so many levels. My dad has a weird hobby. He collects empty bottles which
-    sounds so much better than "alcoholic", doesn't it? I was raised by my father. \n
-    7 : i know this person’s got AIDS, so she should’ve got to see it. She’s got
+    sounds so much better than "alcoholic", doesn't it? I was raised by my father. </li>
+    <li>i know this person’s got AIDS, so she should’ve got to see it. She’s got
     to see for herself, okay? You go to the doctor and he’ll tell you that you’re
     infectious. He’ll think that you’re a horrible person, but that you’re a clever
-    person. \n
-    8 : i'm starting to think you're just like the next person to die, you know,
+    person. </li>
+    <li>i'm starting to think you're just like the next person to die, you know,
     you just go get a job. I mean, I saw one of your videos and I was like, “I’ll
     be the first one to die,” and then I heard somebody calling in sick with a
-    stomach brace. \n
-    9 : did you really think I’m gonna be giving a TED Talk about how the Internet
+    stomach brace. </li>
+    <li>did you really think I’m gonna be giving a TED Talk about how the Internet
     is ruining my life? Do I have to get this over with? I’m gonna tell you what
     I wanna do now! I wanna appeal to the best in society! I’m gonna win this argument.
     This is for you. No one wants to hear about the worst example of sexual
-    harassment they’ve ever had. And you’re right. Second Amendment people! Oh, wait. \n
-    10 : i wish i was young. Now that is a real, real fuck joke. It’s not that
+    harassment they’ve ever had. And you’re right. Second Amendment people! Oh, wait. </li>
+    <li>i wish i was young. Now that is a real, real fuck joke. It’s not that
     I can’t remember it. It’s just that shit has changed. Some of the shit on
     that roast is still in the oven, so, I had to replace the wafer with the
     dryer. You know how that goes. And then, the seats are in the back, the
-    queue is nowhere to be found.''')
+    queue is nowhere to be found.</li></ol>''', unsafe_allow_html=True)
